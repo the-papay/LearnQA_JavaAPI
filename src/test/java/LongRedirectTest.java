fa9d5be9da9dfd152c112d0324825a949ad64418
Vladimir <vssokolov73@gmail.com>
2023-01-08 04:00:45 +0400

    add fix for ex6

--- src/test/java/LongRedirectTest.java
+++ src/test/java/LongRedirectTest.java
@@ -1,15 +1,46 @@
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class LongRedirectTest {
 
     @Test
-    public void testLongRedirectTest() {
+    public void testFindLongRedirectLocationTest() {
 
-        String xHostHeader = RestAssured
+        String locationHeader = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
-                .getHeader("X-Host");
+                .getHeader("Location");
 
-        System.out.println(xHostHeader);
+        System.out.println(locationHeader);
+    }
+
+    @Test
+    public void testLongRedirectUntil200OkReturnedTest() {
+
+        //todo: Наша задача — написать цикл, которая будет создавать запросы в цикле,
+        // каждый раз читая URL для редиректа из нужного заголовка. И так, пока мы не дойдем до ответа с кодом 200.
+        int statusCode = 0;
+        int requestCounter = 0;
+
+        while (statusCode != 200) {
+            String locationHeader = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(true)
+                    .when()
+                    .get("https://playground.learnqa.ru/api/long_redirect")
+                    .getHeader("Location");
+
+            statusCode = RestAssured
+                    .get(locationHeader)
+                    .getStatusCode();
+
+            requestCounter++;
+        }
+        System.out.println(requestCounter);
     }
 }
